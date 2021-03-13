2f67acbe47129fdb889fe40fd66a3cd28876edba
Julia Zolotarev <jzolotarev@gmail.com>
2021-03-12 20:56:36 -0500

    add routes and switch to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar';
+import { Route } from 'react-router';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Opportunity from './Pages/Opportunity';
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* render NavBar */}
-      {/* Switch for Router */}
-      {/* router options */}
+
+      <Switch>
+        <Route to="/about" component={About}/>
+        <Route to="/opportunity" component={Opportunity}/>
+        <Route to="/" component={Home}/>
+      </Switch>
     </div>
   );
 }
