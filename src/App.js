3ef609cc0a1f180014c2ac3a2d4902bb12c91ead
Julia Zolotarev <jzolotarev@gmail.com>
2021-03-12 21:09:57 -0500

    routes working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar';
-import { Route } from 'react-router';
+import { Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
-import About from './Pages/About';
-import Opportunity from './Pages/Opportunity';
+// import About from './Pages/About';
+// import Opportunity from './Pages/Opportunity';
 
 function App() {
   return (
@@ -12,9 +11,9 @@ function App() {
       <NavBar />
 
       <Switch>
-        <Route to="/about" component={About}/>
-        <Route to="/opportunity" component={Opportunity}/>
-        <Route to="/" component={Home}/>
+        {/* <Route to="/about" component={About}/> */}
+        {/* <Route to="/opportunity" component={Opportunity}/> */}
+        <Route path="/" component={Home} />
       </Switch>
     </div>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,3 +2,10 @@
 // buttons for "start here --> sweet pickings"
 // large image
 
+import React from 'react'
+
+export default function Home(){
+    return(
+        <div>testing</div>
+    )
+}
