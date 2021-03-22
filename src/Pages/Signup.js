e10e90ec8880ee10f8c9e289f73698f73fa3e9ba
maasa-k <maasa.dev@gmail.com>
2021-03-21 21:39:28 -0700

    Implement useState in Signup to get values in input fields

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,68 +1,140 @@
-import React from 'react';
+import { React, useState } from 'react';
 
 export default function Signup() {
+    
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    
+    const [address1, setAddress1] = useState('');
+    const [address2, setAddress2] = useState('');
+    const [country, setCountry] = useState('');
+    const [zip, setZip] = useState('');
+    
+    const [email, setEmail] = useState('');
+
+    const [phone1, setPhone1] = useState('');
+    const [phone2, setPhone2] = useState('');
+    const [phone3, setPhone3] = useState('');
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        console.log(`Phone: ${phone1} ${phone2} ${phone3}`);
+    }
+
     return (
         <div>
             <h2>Create Account</h2>
 
-            <form class="ui form">
+            <form className="ui form" onSubmit={handleOnSubmit}>
+                
                 {/* NAME */}
-                <div class="field">
+                <div className="field">
                     <label>Name</label>
-                    <div class="two fields">
-                        <div class="field">
-                            <input type="text" name="first-name" placeholder="First Name" />
+                    <div className="two fields">
+                        <div className="field">
+                            <input 
+                                type="text" 
+                                name="first-name" 
+                                placeholder="First Name" 
+                                onChange={e => setFirstName(e.target.value)}
+                        />
                         </div>
-                        <div class="field">
-                            <input type="text" name="last-name" placeholder="Last Name" />
+                        <div className="field">
+                            <input
+                                type="text" 
+                                name="last-name" 
+                                placeholder="Last Name" 
+                                onChange={e => setLastName(e.target.value)}
+                        />
                         </div>
                     </div>
                 </div>   
 
                 {/* ADDRESS */}
-                <div class="field">
+                <div className="field">
                     <label>Address</label>
-                    <div class="fields">
-                        <div class="twelve wide field">
-                            <input type="text" name="address-1" placeholder="Street Address" />
+                    <div className="fields">
+                        <div className="twelve wide field">
+                            <input 
+                                type="text" 
+                                name="address-1" 
+                                placeholder="Street Address"
+                                onChange={e => setAddress1(e.target.value)}
+                                />
                         </div>
-                        <div class="four wide field">
-                            <input type="text" name="address-2" placeholder="Apt #" />
+                        <div className="four wide field">
+                            <input 
+                                type="text" 
+                                name="address-2" 
+                                placeholder="Apt #" 
+                                onChange={e => setAddress2(e.target.value)}
+                                />
                         </div>
                     </div>
                 </div>
-                <div class="two fields">
-                    <div class="field">
+                <div className="two fields">
+                    <div className="field">
                     <label>Country/Region</label>
-                        <input type="text" name="country-region" placeholder="Country/Region" />
+                        <input 
+                            type="text" 
+                            name="country-region" 
+                            placeholder="Country/Region"
+                            onChange={e => setCountry(e.target.value)}
+                            />
                     </div>
                 </div>
-                <div class="four wide field">
+                <div className="four wide field">
                     <label>Zip/Postal Code</label>
-                    <input id="zip" name="zip" type="text" pattern="[0-9]*" placeholder="Zip/Postal Code" />
+                    <input 
+                        name="zip" 
+                        type="text" 
+                        pattern="[0-9]*" 
+                        placeholder="Zip/Postal Code" 
+                        onChange={e => setZip(e.target.value)}
+                        />
                 </div>
 
                 {/* EMAIL */}
-                <div class="five wide field">
+                <div className="five wide field">
                     <label>Email</label>
-                    <input type="email" name="email" placeholder="Email" />
+                    <input 
+                        type="email" 
+                        name="email" 
+                        placeholder="Email" 
+                        onChange={e => setEmail(e.target.value)}
+                        />
                 </div>
 
                 {/* PHONE */}
-                <div class="inline fields">
+                <div className="inline fields">
                     <label>Phone Number</label>
-                    <div class="field">
-                        <input type="text" placeholder="xxx" maxlength="3" />
+                    <div className="field">
+                        <input 
+                            type="text" 
+                            placeholder="xxx" 
+                            maxLength="3"
+                            onChange={e => setPhone1(e.target.value)}
+                        />
                     </div>
-                    <div class="field">
-                        <input type="text" placeholder="xxx" maxlength="3" />
+                    <div className="field">
+                        <input 
+                            type="text" 
+                            placeholder="xxx"
+                            maxLength="3" 
+                            onChange={e => setPhone2(e.target.value)}
+                            />
                     </div>
-                    <div class="field">
-                        <input type="text" placeholder="xxxx" maxlength="4" />
+                    <div className="field">
+                        <input 
+                        type="text" 
+                        placeholder="xxxx" 
+                        maxLength="4" 
+                        onChange={e => setPhone3(e.target.value)}
+                    />
                     </div>
                 </div>
                 
-                <button type="submit" class="ui green button">Signup</button>
+                <button type="submit" className="ui green button">Signup</button>
             </form>
         </div>
     )
