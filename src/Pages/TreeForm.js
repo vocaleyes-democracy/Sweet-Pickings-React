bf8b7cab5b0084a38e89af607b83be6c62b81cec
maasa-k <maasa.dev@gmail.com>
2021-04-08 22:40:50 -0700

    Create basic Tree Form

--- src/Pages/TreeForm.js
+++ src/Pages/TreeForm.js
@@ -1,106 +1,168 @@
-import React from 'react'
+import { React, useState } from 'react'
+import { Header, Form } from 'semantic-ui-react'
 
 export default function TreeForm() {   
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    
+    const [email, setEmail] = useState('');
+    const [telephone, setTelephone] = useState('');
+    
+    const [address1, setAddress1] = useState('');
+    const [address2, setAddress2] = useState('');
+    const [city, setCity] = useState('');
+    const [country, setCountry] = useState('');
+    const [zip, setZip] = useState('');
+    
+    const treeOptions = [
+        {
+            key: "Apple",
+            text: "Apple",
+            value: "Apple"
+        },
+        {
+            key: "Pear",
+            text: "Pear",
+            value: "Pear"
+        },
+        {
+            key: "Cherry",
+            text: "Cherry",
+            value: "Cherry"
+        },
+        {
+            key: "Walnuts",
+            text: "Walnuts",
+            value: "Walnuts"
+        },
+    ]
+
     return (
         <div id="form">
-            <form class="ui form">
-                <h4 class="ui dividing header">Sweet Pickings Tree Location Form</h4>
+        <Header as="h2" textAlign="center">Sweet Pickings Tree Location Form</Header>
+        
+        <Header as="h3" textAlign="center">Your Contact Information</Header>
+        <Form>
+            <Form.Input 
+                label="First Name" 
+                name="first-name"
+                type="text"
+                placeholder="First Name"
+                onChange={e => setFirstName(e.target.value)} 
+            />
+            <Form.Input 
+                label="Last Name" 
+                name="last-name"
+                type="text"
+                placeholder="Last Name" 
+                onChange={e => setLastName(e.target.value)} 
+            />
+            <Form.Input 
+                label="Email"
+                name="email"
+                type="email"
+                placeholder="sweet.pickings@mail.com"
+                onChange={e => setEmail(e.target.value)}
+            />
+            <Form.Input 
+                label="Telephone"
+                name="telephone"
+                type="tel"
+                placeholder="012-345-6789"
+                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" 
+                maxlength="12"
+                onChange={e => setTelephone}
+            />
+            <Form.Input 
+                label="Address 1"
+                name="address1"
+                type="text"
+                placeholder="Address 1"
+                onChange={e => setAddress1(e.target.value)}
+                />
+            <Form.Input 
+                label="Address 2"
+                name="address2"
+                type="text"
+                placeholder="Address 2"
+                onChange={e => setAddress2(e.target.value)}
+                />
+            <Form.Input 
+                label="City"
+                name="city"
+                type="text"
+                placeholder="City"
+                onChange={e => setCity(e.target.value)}
+                />
+            <Form.Input 
+                label="Country"
+                name="country"
+                type="text"
+                placeholder="Country"
+                onChange={e => setCountry(e.target.value)}
+                />
+            <Form.Input 
+                label="Zip Code"
+                name="zip"
+                type="num"
+                placeholder="Zip Code"
+                onChange={e => setZip(e.target.value)}
+            />
+
+        <Header as="h3" textAlign="center">Tree Information</Header>
+            <Form.Select 
+                label="Select Tree Type"
+                placeholder="Select Tree Type"
+                fluid
+                selection
+                options={treeOptions}
+            />
+            <Form.Input 
+                label="Number of Trees"
+                type="number"
+                name="tree-count"
+                min="1"
+            />
+            <Form.Group grouped>
+                <label>Owner Type</label>
+                <Form.Field 
+                    label="Council Land"
+                    type="radio"
+                    control="input"
+                    name="owner-type"
+                />
+                <Form.Field 
+                    label="Private Property"
+                    type="radio"
+                    control="input"
+                    name="owner-type"
+                />
+                <Form.Field 
+                    label="Not Sure"
+                    type="radio"
+                    control="input"
+                    name="owner-type"
+                />
+                <Form.Field
+                    inline
+                    label="Other"
+                    type="radio"
+                    control="input"
+                    name="owner-type"
+                />
+            </Form.Group>
             
-            {/* Your Name */}
-                <div class="field">
-                    <label>Your Name</label>
-                    <div class="two fields">
-                        <div class="field">
-                            <input type="text" name="first-name" placeholder="First Name" />
-                        </div>
-                        <div class="field">
-                            <input type="text" name="last-name" placeholder="Last Name" />
-                        </div>
-                    </div>
-                </div>
+            <Form.Checkbox 
+                label="I am the owner" 
+            />
+            
+            <Form.TextArea 
+                label="Any additional comments or questions?"
+                placeholder="Tell us more here"
+            />
 
-            {/* Your Email Address */}
-                <div class="field">
-                    <label>Your Email Address</label>
-                    <div class="eight wide field">
-                        <input type="email" name="email" placeholder="human@mail.com" />
-                    </div>
-                </div>
-
-            {/* Telephone */}
-                <div class="field">
-                    <label>Telephone</label>
-                    <div class="five wide field">
-                        <input type="tel" name="telephone" placeholder="012-345-6789" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" maxlength="12" />
-                    </div>
-                </div>
-
-            {/* Tree Location */}
-                <div class="field">
-                    <label>Tree Location</label>
-                    <div class="seven wide field">
-                        <input type="text" name="tree-location" placeholder="Tree Location" />
-                    </div>
-                </div>
-
-            {/* Tree Type */}
-                <div class="field">
-                    <label>Tree Type</label>
-                    <div class="seven wide field">
-                        <input type="text" name="tree-type" placeholder="Tree Type" />
-                    </div>
-                </div>
-
-            {/* Number of Trees */}
-                <div class="field">
-                    <label>Number of Trees</label>
-                    <div class="one wide field">
-                        <input type="number" name="number-of-trees" min="1"  placeholder="Number of Trees" />
-                    </div>
-                </div>
-
-            {/* Owner Type */}
-                <div class="grouped fields">
-                    <label>Owner Type</label>
-                    <div class="field">
-                        <div class="ui radio checkbox">
-                            <input type="radio" name="council-land" />
-                            <label>Council Land</label>
-                        </div>
-                    </div>
-                    <div class="field">
-                        <div class="ui radio checkbox">
-                            <input type="radio" name="private-property" />
-                            <label>Private Property</label>
-                        </div>
-                    </div>
-                    <div class="field">
-                        <div class="ui radio checkbox">
-                            <input type="radio" name="not-sure" />
-                            <label>Not Sure</label>
-                        </div>
-                    </div>
-                    <div class="field">
-                        <div class="ui radio checkbox">
-                            <input type="radio" name="other" />
-                            <div class="inline field">
-                                <label>Other</label>
-                                <input type="text" name="other" placeholder="Description" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            {/* Checkbox for "I am the owner" */}
-
-            {/* Owner Name */}
-
-            {/* Owner Email Address */}
-
-            {/* Owner Telephone */}
-
-            {/* Any additional comments or questions? */}
-            </form>
+            <Form.Button type="submit">Submit</Form.Button>
+        </Form>
         </div>
     )
 }
