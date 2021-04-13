68810c3ae20a117237569981cabeb1c586a10017
miShelbyT <mi.shelbyrose@gmail.com>
2021-04-12 22:12:18 -0400

    tree list rendered from db.json

---
+++ src/Pages/Tree.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { List } from 'semantic-ui-react'
+
+function Tree({ tree }){
+
+  return(
+    <List>
+    <List.Item>
+      <List.Icon name='tree' />
+      <List.Content>Tree #{tree.id}</List.Content>
+      <List.Content>Variety: {tree.variety}</List.Content>
+      <List.Content>Address #1: {tree.address1}</List.Content>
+      <List.Content>Address #2: {tree.address2}</List.Content>
+    </List.Item>
+    </List>
+  )
+}
+
+export default Tree
