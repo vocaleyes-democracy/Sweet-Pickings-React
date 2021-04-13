d7c627f5f99a50d71864b18e7fa4a796b0e561da
miShelbyT <mi.shelbyrose@gmail.com>
2021-04-13 18:38:11 -0400

    updated/tweaked tree list

--- src/Pages/Tree.js
+++ src/Pages/Tree.js
@@ -1,18 +1,31 @@
 import React from 'react'
 import { List } from 'semantic-ui-react'
 
-function Tree({ tree }){
+function Tree({ tree }) {
 
-  return(
-    <List>
-    <List.Item>
-      <List.Icon name='tree' color='green' />
-      <List.Content>Tree #{tree.id}</List.Content>
-      <List.Content>Variety: {tree.variety}</List.Content>
-      <List.Content>Number of Trees: {tree.numberOfTrees}</List.Content>
-      <List.Content>Address #1: {tree.address1}</List.Content>
-      <List.Content>Address #2: {tree.address2}</List.Content>
-    </List.Item>
+  return (
+    <List as="h3" className="list-item">
+      <List.Item className="list-item">
+        <List.Icon name='leaf' color='green' />
+        <List.Content>
+          <List.Header>#{tree.id}</List.Header>
+        </List.Content>
+        <List.Content>
+          <List.Description>Type: {tree.type}</List.Description>
+        </List.Content>
+        <List.Content>
+          <List.Description>Variety: {tree.variety}</List.Description>
+        </List.Content>
+        <List.Content>
+          <List.Description>Number of Assets: {tree.numberOfTrees}</List.Description>
+        </List.Content>
+        <List.Content>
+          <List.Description>Address #1: {tree.address1}</List.Description>
+        </List.Content>
+        <List.Content>
+          <List.Description>Address #2: {tree.address2}</List.Description>
+        </List.Content>
+      </List.Item>
     </List>
   )
 }
--- src/Pages/TreeList.js
+++ src/Pages/TreeList.js
@@ -4,13 +4,12 @@ import Tree from './Tree.js'
 function TreeList({ trees }){
 
   const renderTrees = () => {
-    // console.log(trees)
     return trees.map(tree => <Tree key={tree.id} tree={tree}/> )
   } 
 
   return(
     <div className="tree-page">
-      <h1>Grower Tree List:</h1>
+      <h1>Assets:</h1>
       {renderTrees()}
     </div>
   )
