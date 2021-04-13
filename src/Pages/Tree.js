e8a6535a74cdf6619265ac28b94ed5c4d906141e
miShelbyT <mi.shelbyrose@gmail.com>
2021-04-12 22:53:31 -0400

    adjusted tree list

--- src/Pages/Tree.js
+++ src/Pages/Tree.js
@@ -6,9 +6,10 @@ function Tree({ tree }){
   return(
     <List>
     <List.Item>
-      <List.Icon name='tree' />
+      <List.Icon name='tree' color='green' />
       <List.Content>Tree #{tree.id}</List.Content>
       <List.Content>Variety: {tree.variety}</List.Content>
+      <List.Content>Number of Trees: {tree.numberOfTrees}</List.Content>
       <List.Content>Address #1: {tree.address1}</List.Content>
       <List.Content>Address #2: {tree.address2}</List.Content>
     </List.Item>
