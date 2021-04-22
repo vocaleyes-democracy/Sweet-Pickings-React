6d08c82b4181d5ba828af57d3f4c594931b05f3d
miShelbyT <mi.shelbyrose@gmail.com>
2021-04-22 13:45:20 -0400

    markers rendered yayyyyyy

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -2,54 +2,26 @@ import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { sendGeocodingRequest } from '../redux/actions'
-import { APPLICATION_ID, API_KEY } from '../APIs/travelTimeAPI'
+// import { APPLICATION_ID, API_KEY } from '../APIs/travelTimeAPI'
 
 
 
 function Map(props) {
 
-  // i want to be able to iterate thru props.assets and use my geoData function to return long/lat for each asset so i can set Markers. Right now I have props.getGeodata (my action) which publishes state to the store, but just one time (line 14 below). when i set msp the compiler goes into an infinite GET loop. NOTE: i can't pass an array of addresses to this function because the address is part of the endpoint that i need for my GET request
+
+  const [map, setMap] = useState({ lat: 51.62284989072822, lng: -3.943877083337367, zoom: 11, city: "Swansea" })
 
 
-  const [map, setMap] = useState({ lat: 40.744385, lng: -73.9573038, zoom: 11, city: "LIC" })
-  const [geo, setGeo] = useState({ address: "Buckingham Palace Road, London, UK" })
-  const [array, setArray] = useState([])
-
-
-  const addressArray = props.assets.map(asset => `${asset.address1}, ${asset.address2}`)
-  console.log(addressArray) // address array - success!
-
-  // function useSetCoordinates(){
-  //   const [long, latt] = props.coordinates
-  //   setMap({lat: latt, lng: long, zoom: 11, city: "London" })
-  // }
-
-  useEffect(() => {
-    props.geoData(geo.address)
-  }, [])
-
-  // useEffect(() => {
-  //   addressArray.map(address => {
-  //     fetch(`https://api.traveltimeapp.com/v4/geocoding/search?query=` + address, {
-  //       method: "GET",
-  //       credentials: "same-origin",
-  //       headers: {
-  //       "Content-Type": "application/json",
-  //       "X-Application-Id": APPLICATION_ID,
-  //       "X-Api-Key": API_KEY,
-  //       "Accept-Language": "en-GB"
-  //       }
-        
-  //       })
-  //       .then(response => response.json()) // parses JSON response into native Javascript objects
-  //       .then(data => {
-  //         setArray(data.features)
-  //         console.log(array)
-  //       })
-  //   })
-  // },[])
-
-  
+  const renderMarkers = () => {
+    return props.assets.map(asset => {
+      // console.log(asset)
+      return (<Marker  key={asset.id} position={[asset.coordinates[1], asset.coordinates[0]]}>
+        <Popup>
+          Asset #{asset.id}
+    </Popup>
+      </Marker>)
+    })
+  }
 
 
   return (
@@ -62,12 +34,7 @@ function Map(props) {
           attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-
-        <Marker position={[map.lat, map.lng]}>
-          <Popup>
-            You are in {map.city}!
-        </Popup>
-        </Marker>
+        {renderMarkers()}
       </MapContainer>
     </>
 
