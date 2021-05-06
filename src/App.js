4a0f542ac562fb3e8c710dfc8621edce4b5c26a1
maasa-k <maasa.dev@gmail.com>
2021-05-05 17:49:36 -0700

    Start implementing Material UI

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import TopNav from './Components/TopNav'
 // import HeaderImg from './Components/HeaderImg'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
+// Materia UI
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+
 import Home from './Pages/Home'
 import About from './Pages/About/About.js'
 // import Signup from './Pages/Signup';
@@ -19,74 +22,95 @@ import Login from './Pages/auth/Login'
 import Signup from './Pages/auth/Signup'
 // import Gallery from './Pages/About/Gallery'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#a2cf6e',
+      main: '#8bc34a',
+      dark: '#618833',
+      contrastText: '#fff',
+    },
+    // secondary: {
+    //   light: '#ff7961',
+    //   main: '#f44336',
+    //   dark: '#ba000d',
+    //   contrastText: '#000',
+    // },
+  },
+});
+
 function App() {
   return (
     <div>
       <div className="navbar-container">
-      <Router>
-        <TopNav />
-        <NavBar2 />
-    
-        <Switch>
+      <ThemeProvider theme={theme}>
+          <Router>
+            {/* <Layout> */}
+            <TopNav />
+            <NavBar2 />
+        
+            <Switch>
 
-          <Route exact path="/">
-            <Redirect to="/home" />
-          </Route>
+              <Route exact path="/">
+                <Redirect to="/home" />
+              </Route>
 
-          <Route exact path="/home">
-            <Home />
-          </Route>
+              <Route exact path="/home">
+                <Home />
+              </Route>
 
-          <Route exact path="/about">
-            <About />
-          </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
 
-          <Route exact path="/story">
-            <Story />
-          </Route>
+              <Route exact path="/story">
+                <Story />
+              </Route>
 
-          <Route exact path="/pickers">
-            <Pickers />
-          </Route>
+              <Route exact path="/pickers">
+                <Pickers />
+              </Route>
 
-          <Route exact path="/growers">
-            <Growers />
-          </Route>
-          
-          <Route exact path="/recipients">
-            <Recipients />
-          </Route>
-          
-          <Route exact path="/tree-form">
-            <TreeForm />
-          </Route>
-          
-          <Route exact path="/contact">
-            <Contact />
-          </Route>
-          
-          <Route exact path="/opportunities">
-            <Opportunities />
-          </Route>
-          
-          <Route exact path="/login">
-            <Login />
-          </Route>
-          
-          <Route exact path="/signup">
-            <Signup />
-          </Route>
-          
-          {/* Commented out Signup component to stop it from rendering everywhere */}
-          {/* <Route>
-            <Signup exact path="/signup" />
-          </Route> */}
-          {/* <Route>
-            <Gallery exact path="/gallery" />
-          </Route> */}
+              <Route exact path="/growers">
+                <Growers />
+              </Route>
+              
+              <Route exact path="/recipients">
+                <Recipients />
+              </Route>
+              
+              <Route exact path="/tree-form">
+                <TreeForm />
+              </Route>
+              
+              <Route exact path="/contact">
+                <Contact />
+              </Route>
+              
+              <Route exact path="/opportunities">
+                <Opportunities />
+              </Route>
+              
+              <Route exact path="/login">
+                <Login />
+              </Route>
+              
+              <Route exact path="/signup">
+                <Signup />
+              </Route>
+              
+              {/* Commented out Signup component to stop it from rendering everywhere */}
+              {/* <Route>
+                <Signup exact path="/signup" />
+              </Route> */}
+              {/* <Route>
+                <Gallery exact path="/gallery" />
+              </Route> */}
 
-          </Switch>
-        </Router>
+              </Switch>
+            {/* </Layout> */}
+            </Router>
+        </ThemeProvider>
       </div>
     </div>
   );
--- src/Components/TopNav.js
+++ src/Components/TopNav.js
@@ -1,35 +1,61 @@
 import React from 'react'
-// import SideBar from './SideBar'
 import { NavLink } from 'react-router-dom'
-import { Menu, Search, Item, Icon } from 'semantic-ui-react'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles((theme) => ({
+
+}));
 
 export default function TopNav(){
     return (
-            <Menu
-                // fixed="top"  // seems like we can also use "attached" if we add a second header piece
-                attached="top"
-                inverted 
-                size="tiny" 
-                color="olive" // need to figure out how to add primary color #7FA11C
-                stackable
-                borderless
-                secondary
-                // widths="1"
-                // text
-                // compact
-                position="left"
-            >
-                <Menu.Item>
-                    <h4 >Grow for us, pick with us</h4>
-                </Menu.Item>
-
-                <Menu.Item>
-                    <p> United Kingdom, TXB273KE8</p>
-                </Menu.Item>
-
-                <Menu.Item>
-                    <p>help@sweetpickings.com</p>
-                </Menu.Item>
-            </Menu>
+        <div>
+        <AppBar 
+            position="static"
+            color="primary"
+        >
+            <Toolbar>
+                <Typography variant="h6">
+                    Grow for us, pick with us
+                </Typography>
+                <Typography>
+                    United Kingdom, TXB273KE8
+                </Typography>
+                
+                <Typography>
+                    help@sweetpickings.com
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        </div>
     )
 }
+
+{/* <Menu
+    // fixed="top"  // seems like we can also use "attached" if we add a second header piece
+    attached="top"
+    inverted 
+    size="tiny" 
+    color="olive" // need to figure out how to add primary color #7FA11C
+    stackable
+    borderless
+    secondary
+    // widths="1"
+    // text
+    // compact
+    position="left"
+>
+    <Menu.Item>
+        <h4 >Grow for us, pick with us</h4>
+    </Menu.Item>
+
+    <Menu.Item>
+        <p> United Kingdom, TXB273KE8</p>
+    </Menu.Item>
+
+    <Menu.Item>
+        <p>help@sweetpickings.com</p>
+    </Menu.Item>
+</Menu> */}
