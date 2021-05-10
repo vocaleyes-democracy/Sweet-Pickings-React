ed80d963f39ae28278289667d51ccfd9893951f9
maasa-k <maasa.dev@gmail.com>
2021-05-10 15:27:19 -0700

    Add skeletal information on About subpages

--- src/Pages/About/Pickers.js
+++ src/Pages/About/Pickers.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
 
 export default function Pickers() {
     return (
-        <div>
-            Pickers About Page
-        </div>
+        <Container>
+            <Typography
+                variant={"h3"}
+                color={"primary"}
+            >
+                PICKERS:
+            </Typography>
+            <Typography
+                variant={"secondaryText"}
+                align={"center"}
+            >
+                Information about volunteering as a picker, the picking process etc.
+
+                numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur
+            </Typography>
+        </Container>
     )
 }
--- src/Pages/About/Story.js
+++ src/Pages/About/Story.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
 
 export default function Story() {
     return (
-        <div>
-            The Story About Page
-        </div>
+        <Container>
+            <Typography
+                variant={"h3"}
+                color={"primary"}
+            >
+                STORY:
+            </Typography>
+            <Typography
+                variant={"secondaryText"}
+                align={"center"}
+            >
+                Information about the Sweet Pickings concept and story goes here...
+
+                numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur
+            </Typography>
+        </Container>
     )
 }
