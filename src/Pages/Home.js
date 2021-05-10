bef2573518c37c4c78c538e1fc00361d205adfdf
miShelbyT <mi.shelbyrose@gmail.com>
2021-05-10 12:42:34 -0400

    updated home page using flexbox on home/image div

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -14,19 +14,14 @@ import Typography from '@material-ui/core/Typography'
 const useStyles = makeStyles({
   btn: {
     fontSize: 20,
-    marginTop: 550,
+    marginTop: '5%',
     '&:hover': {
       backgroundColor: '#868d23',
       color: 'white'
     },
   },
   title: {
-    // fontSize: 70,
-    fontWeight: 'bold',
-    // color: '#000',
-    paddingTop: '15%',
-    display: 'inline-block',
-    float: 'right'
+    fontWeight: 'bold'
   },
   sweet: {
     color: '#404A3D'
@@ -37,14 +32,16 @@ const useStyles = makeStyles({
 export default function Home() {
 const classes = useStyles()
     return (
+      <>
         <div className={'home'}>
           <Typography variant="h2"  color={'secondary'} className={classes.title}>
-            PICKINGS
-          </Typography>
-          <Typography variant="h2" align='center' className={`${classes.title} ${classes.sweet}`}>
             SWEET
           </Typography>
+          <Typography variant="h2" align='center' className={`${classes.title} ${classes.sweet}`}>
+            PICKINGS
+          </Typography>
           {/* <HeaderImg /> */}
+          </div>
           <Container align='center'>
             <Button 
             variant="contained"
@@ -79,7 +76,7 @@ const classes = useStyles()
                     <Link to="/about">Get Started</Link>
                 </Button>
             </Container> */}
-        </div>
+        </>
     )
 }
  
