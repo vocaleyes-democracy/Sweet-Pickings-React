d1bc858656bd8b7dd02a9df391c83ecb202ac5d4
miShelbyT <mi.shelbyrose@gmail.com>
2021-06-01 16:42:19 -0400

    got rid of one or two imported components we weren't using

--- src/Components/Tree.js
+++ src/Components/Tree.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import Card from '@material-ui/core/Card'
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-// import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import clsx from 'clsx';
@@ -18,7 +16,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import apples from '../images/apples2.png'
 
 
-// import { List } from 'semantic-ui-react'
 
 const useStyles = makeStyles((theme) => ({
   
