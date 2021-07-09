1d68c1844d9516f95fc33a4c8c490565007ad129
miShelbyT <mi.shelbyrose@gmail.com>
2021-07-09 12:11:26 -0400

    added info icons to tree cards that link to show page, onClick

--- src/Components/TreeCard.js
+++ src/Components/TreeCard.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core/styles';
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined'
+import Link from '@material-ui/core/Link'
+import { makeStyles } from '@material-ui/core/styles'
 
 // fruit card images
 import apple from '../images/cardImgApples.jpg'
@@ -16,100 +18,111 @@ import cherry from '../images/cardImgCherries.jpg'
 import genericFruit from '../images/cardImgFruit.jpg'
 
 const useStyles = makeStyles((theme) => ({
-  
   card: {
     maxWidth: 545,
     height: 300,
-    overflow: "auto",
-    marginBottom: 35
+    overflow: 'auto',
+    marginBottom: 35,
   },
 
   header: {
-    padding: 2
+    padding: 2,
   },
 
   media: {
-    height: 140
+    height: 140,
   },
   mediaTitle: {
     backgroundColor: 'rgba(237, 226, 225, 0.5)',
     color: '#423e3e',
   },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
+  info: {
+    marginTop: theme.spacing(2),
+    float: 'right'
   },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  }
-}));
-
+  
+}))
 
 function TreeCard({ tree }) {
   const classes = useStyles()
 
-// const [cardFlip, setCardFlip] = React.useState(false);
+  // const [cardFlip, setCardFlip] = React.useState(false);
 
-// this handleClick function will take user to show page of site/tree
+  // this handleClick function will take user to show page of site/tree
   const handleClick = () => {
     console.log('Tree ID: ', tree.id)
-  };
+  }
 
   // there is an issue here where if there is more than one asset we aren't iterating thru and returning any img. using .find should return the first url but i haven't worked it out yet. For now our data only shows one fruit per site
   function renderFruitImage(fruit) {
-
-    switch(fruit) {
+    switch (fruit) {
       case 'Blueberry':
       case 'Blueberries':
-        return blueberries;
+        return blueberries
       case 'Apple':
       case 'Apples':
-        return apple;
+        return apple
       case 'Cherry':
       case 'Cherries':
-        return cherry;
+        return cherry
       case 'Pear':
       case 'Pears':
-        return pear;
+        return pear
       default:
-        return genericFruit;
+        return genericFruit
     }
   }
-   
 
   return (
     <Container>
-      <Card elevation={2} className={classes.card} style={{backgroundColor: '#7FA11C'}} onClick={handleClick}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          // renders first fruit regardless of how many fruit types. for now. we'll see if this still works when we add additional fruit types
-          image={renderFruitImage(tree.asset[0].variety)}
-          title="Produce">
-
-          <Typography key={tree.id} variant="h4" align="center" className={classes.mediaTitle}>{tree.address1}</Typography>
-
+      <Card
+        elevation={2}
+        className={classes.card}
+        style={{ backgroundColor: '#7FA11C' }}
+        onClick={handleClick}
+      >
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            // renders first fruit regardless of how many fruit types. for now. we'll see if this still works when we add additional fruit types
+            image={renderFruitImage(tree.asset[0].variety)}
+            title="Produce"
+          >
+            <Typography
+              key={tree.id}
+              variant="h4"
+              align="center"
+              className={classes.mediaTitle}
+            >
+              {tree.address1}
+            </Typography>
           </CardMedia>
-        <CardContent>
-          <CardHeader className={classes.header} align='center'
-          title={`Site# ${tree.id}`}
-          />
+          <CardContent>
+            <CardHeader
+              className={classes.header}
+              align="center"
+              title={`Site# ${tree.id}`}
+            />
 
-          <Typography variant="body2" color="inherit" align='center' gutterBottom={true}>
-          Variety: {tree.asset.map(treeObj => treeObj.variety).join(', ')}
-          </Typography>
+            <Typography
+              variant="body2"
+              color="inherit"
+              align="center"
+              gutterBottom={true}
+            >
+              Variety: {tree.asset.map((treeObj) => treeObj.variety).join(', ')}
+            </Typography>
 
-          <Typography variant="body2" color="textSecondary" align='center'>
-          {`Location: ${tree.address2}`}
-          </Typography>
-
-        </CardContent>
-      </CardActionArea>
+            <Typography variant="body2" color="textSecondary" align="center">
+              {tree.address2}
+            </Typography>
+            <Link className={classes.info} href={`/opportunities/${tree.id}`}>
+            <InfoOutlinedIcon fontSize="large" color='secondary'></InfoOutlinedIcon>
+            </Link>
+          </CardContent>
+        </CardActionArea>
       </Card>
-  </Container>
+    </Container>
   )
 }
 
