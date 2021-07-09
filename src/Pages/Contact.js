7981d0e7bebb0fa58830af78294469ff61cbab10
miShelbyT <mi.shelbyrose@gmail.com>
2021-07-08 21:25:56 -0400

    added coming soon to Contact

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core'
+
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '5%',
+    },
+})
+
 export default function Contact() {
+
+  const classes = useStyles()
     return (
-        <div>
-            CONTACT INFORMATION
-        </div>
+        <Container className={classes.root}>
+            <Typography variant="h4">COMING SOON</Typography>
+        </Container>
     )
 }
